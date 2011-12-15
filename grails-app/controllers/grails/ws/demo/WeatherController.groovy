5cec72900b6381aaf0ad291a43c58f4a264ef032
Christopher Johnson <chris@clickonchris.com>
2011-12-14 23:04:27 -0600

    formatting

--- grails-app/controllers/grails/ws/demo/WeatherController.groovy
+++ grails-app/controllers/grails/ws/demo/WeatherController.groovy
@@ -10,21 +10,21 @@ class WeatherController {
 	def index = {
 		def message
 
-        if (params?.value && params.from) {
-		
-			def proxy = WebServiceClientFactory.getWeatherService()
-            def result
-			def to_units
-            if ("Celcius".equals(params.from)) {
-                result = proxy.CelsiusToFahrenheit(params.value)
-				to_units =  "F"
-            } else {
-                result = proxy.FahrenheitToCelsius(params.value)
-				to_units = "C"
-            }
-            message = "${params.value} degrees ${params.from} is ${result} degrees ${to_units}"
-        }
+		if (params?.value && params.from) {
 
-        flash.message = message
+			def proxy = WebServiceClientFactory.getWeatherService()
+			def result
+			def to_units
+			if ("Celcius".equals(params.from)) {
+				result = proxy.CelsiusToFahrenheit(params.value)
+				to_units =  "F"
+			} else {
+				result = proxy.FahrenheitToCelsius(params.value)
+				to_units = "C"
+			}
+			message = "${params.value} degrees ${params.from} is ${result} degrees ${to_units}"
+		}
+
+		flash.message = message
 	}
 }
